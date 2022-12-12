2ac48a0d41bc1a6bb136115fba51c84290da6157
Raphaël Diacamille <49157684+rdia9@users.noreply.github.com>
2022-12-12 15:26:41 +0100

    Delete disable-spooler-lotX.ps1

--- disable-spooler-lotX.ps1
+++
@@ -1,7 +0,0 @@
-$servicename = "spooler"
-$serveurs = Get-Content "serveurs_spooler_lot_p3.csv"
-
-(Invoke-Command -ComputerName $serveurs -ScriptBlock {Get-Service $using:servicename}) | Format-Table PSComputerName, name, status, StartType
-Invoke-Command -ComputerName $serveurs -ScriptBlock {stop-Service $using:servicename} 
-Invoke-Command -ComputerName $serveurs -ScriptBlock {set-Service $using:servicename -StartupType disabled} 
-(Invoke-Command -ComputerName $serveurs -ScriptBlock {Get-Service $using:servicename}) | Format-Table PSComputerName, name, status, StartType
