d7607d46879cd33fc8777f9fbec834e841ca5dee
Raphaël Diacamille <raphael.diacamille@btp-consultants.fr>
2024-09-19 14:47:37 +0200

    Delete Deco-InactiveTerminalSessions.ps1

--- Deco-InactiveTerminalSessions.ps1
+++
@@ -1,5 +0,0 @@
-# Spécifie un délai d'inactivité avant déconnexion (30 minutes)
-$timeOutValue = 1800
-
-# Appliquer à la configuration du serveur
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name IdleWinStationPoolTimeout -Value $timeOutValue
