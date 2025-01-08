5558f042da49b18f4adfb3efc4717a6e8e1c26aa
Raphael Diacamille <raphael.diacamille@btp-consultants.fr>
2025-01-08 14:28:42 +0100

    delete files

--- Report-AD-OS-count-list.ps1
+++
@@ -1,3 +0,0 @@
-import-module activedirectory
-$Computers = @(Get-ADComputer -Properties Name,operatingSystem -Filter {(OperatingSystem -like "*")})
-$Computers | Group-Object operatingSystem | Select Count, Name | Sort Name
--- Report-AD-OS-list.ps1
+++
@@ -1,3 +0,0 @@
-import-module activedirectory
-$Computers = @(Get-ADComputer -Properties Name,operatingSystem -Filter {(OperatingSystem -like "*")})
-$Computers  | Select operatingSystem, Name | Sort operatingSystem | Export-Csv Report-AD-OS-list.csv
